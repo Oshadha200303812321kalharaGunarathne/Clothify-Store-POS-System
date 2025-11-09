e74587726d7aef37a25272b3d65edfde7013f238
Oshadha Kalhara Gunarathne <Oshadha200303812321kalharaGunarathne@gmail.com>
2025-11-09 17:38:50 +0530

    Add Database Connection

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -1,4 +1,23 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    public static DBConnection instance;
+
+    private Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ClothifyStore","root","1234");
+    }
+
+    private DBConnection getInstance() throws SQLException {
+        return instance == null? instance = new DBConnection():instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
