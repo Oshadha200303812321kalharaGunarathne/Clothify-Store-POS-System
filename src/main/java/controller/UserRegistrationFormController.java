c9df9f6c945e9c913bf9c9fa70830070f877a875
Oshadha Kalhara Gunarathne <Oshadha200303812321kalharaGunarathne@gmail.com>
2025-11-11 16:12:55 +0530

    Insert codes

--- src/main/java/controller/UserRegistrationFormController.java
+++ src/main/java/controller/UserRegistrationFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class UserRegistrationFormController {
+
+    @FXML
+    private JFXButton btnRegister;
+
+    @FXML
+    private JFXTextField txtConfirmPassword;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
 }
+
