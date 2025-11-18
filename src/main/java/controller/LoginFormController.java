d4bba00e01372fc21f4317bdc76c6d808f1e262e
Oshadha Kalhara Gunarathne <Oshadha200303812321kalharaGunarathne@gmail.com>
2025-11-19 00:09:12 +0530

    Add Code to Controller Class

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lblMouseClickOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void lblRegisterMouseClickOnAction(MouseEvent event) {
+
+    }
+
 }
