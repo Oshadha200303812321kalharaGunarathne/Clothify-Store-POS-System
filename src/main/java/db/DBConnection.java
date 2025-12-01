f3664f7e9c45aca892a88f760026b248e7530e6a
Oshadha Kalhara Gunarathne <Oshadha200303812321kalharaGunarathne@gmail.com>
2025-12-01 23:15:38 +0530

    Update

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
     private Connection connection;
 
     private DBConnection() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ClothifyStore","root","1234");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothifyStore","root","1234");
     }
 
     public static DBConnection getInstance() throws SQLException {
