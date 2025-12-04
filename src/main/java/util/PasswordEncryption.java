9982f7d6e03bf28902cfc3429e171c7fa72ae701
Oshadha Kalhara Gunarathne <Oshadha200303812321kalharaGunarathne@gmail.com>
2025-12-04 11:05:48 +0530

    password Encryption

---
+++ src/main/java/util/PasswordEncryption.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.security.MessageDigest;
+
+public class PasswordEncryption {
+
+    public static String encrypt(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+
+            byte[] bytes = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
